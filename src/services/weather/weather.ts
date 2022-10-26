9356710345ed06cf30fbd76695df0d215db46a3f
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-10-26 18:48:24 +0300

    get location function implemented

--- src/services/weather/weather.ts
+++ src/services/weather/weather.ts
@@ -1,11 +1,13 @@
 import {HttpServices} from '../http/http';
 import {HttpUrlPath} from '~/common/enums/enums';
+import {getCurrentCity} from '~/helpers/helpers';
 
 class WeatherServices {
   private http: HttpServices = new HttpServices();
-  getWeather(requestParams = {}) {
+  async getWeather(requestParams = {}) {
+    const city = await getCurrentCity()
     return this.http.load(`${HttpUrlPath.BASE}`, {
-      params: {...requestParams},
+      params: {...requestParams, q:city, days:7},
     });
   }
 }
