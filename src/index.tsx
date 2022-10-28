db04598e18bfb3f68f4292b736fb186682146eda
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-10-28 15:32:49 +0300

    file structure changed

---
+++ src/index.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { Navigation } from "~/navigation/navigation";
+import { useFonts, useCallback } from "~/hooks/hooks";
+import { store, StoreContext } from "~/store/store";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+const App: FC = () => {
+  let [fontsLoaded] = useFonts({
+    "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
+    "Inter-Medium": require("./assets/fonts/Inter-Medium.ttf"),
+    "Inter-SemiBold": require("./assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-Bold": require("./assets/fonts/Inter-Bold.ttf"),
+    "Inter-ExtraBold": require("./assets/fonts/Inter-ExtraBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <StoreContext.Provider value={store}>
+      <NavigationContainer onReady={onLayoutRootView}>
+        <Navigation />
+      </NavigationContainer>
+    </StoreContext.Provider>
+  );
+};
+
+export { App };
